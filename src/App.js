306d72216099a988e4980d90a8687a728d7b7519
348670050 <haloday873@gmail.com>
2023-01-21 01:07:44 -0500

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Pokedex from './pages/Pokedex.js';
+
+class App extends Component {
+
+    render() {
+        return (
+            <React.StrictMode>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/dex" element={<Pokedex />} />
+                        <Route path="/*" element={<Pokedex />} />
+                    </Routes>
+                </Router>
+            </React.StrictMode>
+        )
+    }
+}
+
+export default App;
---
+++ src/pages/Pokedex.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+class Pokedex extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>ASD</h1>
+            </div>
+        )
+    }
+}
+
+export default Pokedex;
